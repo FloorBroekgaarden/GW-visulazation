d3321a288e80d6881992bf04518174738f718345
Floor Broekgaarden <fsbroekgaarden@gmail.com>
2021-05-08 14:06:59 -0400

    updated movie

--- makeMovie.py
+++ makeMovie.py
@@ -10,7 +10,7 @@ import glob
 
 
 
-def makeMovie_GW(image_folder = '/Users/floorbroekgaarden/Projects/GitHub/GW-visulazation/movie_BHmass_spin/',fps=.4, duration=400, name='movie_BHmass_spin'):
+def makeMovie_GW(image_folder = '/Users/floorbroekgaarden/Projects/GitHub/GW-visulazation/movie_BHmass_spin/',fps=.4, duration=400, name='movie_BHmass_spin', img_range=259):
 	'''
 	whichRate = 'intrinsic' or 'observed'
 	fps=0.4, frames per second
@@ -25,7 +25,7 @@ def makeMovie_GW(image_folder = '/Users/floorbroekgaarden/Projects/GitHub/GW-vis
 	
 
 	# automatize to length -1 no of files. 
-	for f_ind  in range(219):
+	for f_ind  in range(img_range):
 		images.append(image_folder +  'img_'  + str(f_ind) + '.png')
 
 
@@ -55,9 +55,10 @@ def makeMovie_GW(image_folder = '/Users/floorbroekgaarden/Projects/GitHub/GW-vis
 
 
 
-Movie_GW_BHmassVSspin =True 
-Movie_GW_BHmassVSradiusISCO = True
-
+Movie_GW_BHmassVSspin = False 
+Movie_GW_BHmassVSradiusISCO = False
+Movie_GW_BHmassVSradius  =True 
+Movie_GW_BHmassVSradius_NL=True
 
 # Run rhis using python 3!! 
 
@@ -66,6 +67,14 @@ if Movie_GW_BHmassVSspin==True:
 	
 if Movie_GW_BHmassVSradiusISCO==True:
 	makeMovie_GW(image_folder='/Users/floorbroekgaarden/Projects/GitHub/GW-visulazation/movie_BHmass_RadiusISCO/', fps=.4, duration=300, name='movie_BHmass_RadiusISCO')
+
+
+if Movie_GW_BHmassVSradius==True:
+	makeMovie_GW(image_folder='/Users/floorbroekgaarden/Projects/GitHub/GW-visulazation/movie_BHmass_Rs/', fps=2, duration=300, name='movie_BHmass_Rs',img_range=230)
+
+
+if Movie_GW_BHmassVSradius_NL==True:
+	makeMovie_GW(image_folder='/Users/floorbroekgaarden/Projects/GitHub/GW-visulazation/movie_BHmass_Rs_NL/', fps=2, duration=300, name='movie_BHmass_Rs_NL',img_range=230)
 
 
 
